af7be05234a7df8d5d4187252441031f345fd8b9
BrunoFinhana <14461897-BrunoFinhana@users.noreply.replit.com>
2023-08-02 14:01:30 +0000

    Refinamento do código para apresentação final.

--- Pessoa.java
+++ Pessoa.java
@@ -1,27 +1,27 @@
 public class Pessoa {
 
-	// Atributos
+	// ATRIBUTOS
   private String nome = "Bruno";
 	private int dataNasc = 21021997;
 	private int idade = 26;
 
-	// Metodos construtores
+	// MÉTODOS CONSTRUTORES
 
 	// Getter e Setter para nome
 	public String getNome()
 	{
 		return nome;
-}
+	}
 	public void setNome(String n)
 	{
     this.nome = n;
-}
+	}
 
 	// Getter e Setter para data de nascimento
 	public int getDataNasc() 
 	{
 		return dataNasc;
-}
+	}
 	public void setNome(int dn)
 	{
     this.dataNasc = dn;
@@ -31,9 +31,10 @@ public class Pessoa {
 	public int getIdade()
 	{
 		return idade;
-}
+	}
 	public void setIdade(int i)
 	{
     this.idade = i;
-}
 	}
+
+}
